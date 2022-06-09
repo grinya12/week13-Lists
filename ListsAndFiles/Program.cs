734a628293aad473c42933d755f391908fef1cb4
Grigori Gritsenko <grissha2002@gmail.com>
2022-06-09 13:10:40 +0300

    week13

---
+++ ListsAndFiles/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ListsAndFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"/Users/GRI$$$HA/Projects/Lists/Data/fruit.txt";
+            List<string> fileContent = File.ReadAllLines(filePath).ToList();
+
+            Console.WriteLine("Original file content>");
+            DisplayListElements(fileContent);
+
+            Console.WriteLine("Update list content");
+            //fileContent.Add("almond");
+            //DisplayListElements(fileContent);
+
+            int indexOfTomato = fileContent.IndexOf("tomato");
+            Console.WriteLine($"Tomato is located at index {indexOfTomato}");
+            //fileContent.RemoveAt(indexOfTomato);
+
+            DisplayListElements(fileContent);
+
+            File.WriteAllLines(filePath, fileContent);
+        }
+
+        public static void DisplayListElements(List<string> someList)
+        {
+            foreach(string element in someList)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fruitSalad = new List<string>();
+
+            fruitSalad.Add("yogurt");
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Enter the ingredient {i + 1}:");
+                string userIngredient = Console.ReadLine();
+                fruitSalad.Add(userIngredient);
+            }
+
+            Console.WriteLine($"Your salad consists of: {fruitSalad.Count} ingredients.");
+
+            DisplayIngredients(fruitSalad);
+
+        }
+
+        public static void DisplayIngredients(List<string> someList)
+        {
+            foreach (string ingredient in someList)
+            {
+                Console.WriteLine(ingredient);
+            }
+        }
+    }
+}
